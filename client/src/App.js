13d2ec45dbd54ddb0134f9b5470dd92c956b1022
Harry <harrisonminer59@gmail.com>
2023-10-12 13:48:02 -0400

    fixed authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, concat } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
+// Create an http link to connect to your GraphQL server
+const httpLink = createHttpLink({
   uri: 'http://localhost:3000/graphql',
+});
+
+// Create an auth link to add the token to request headers
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // Return the headers to the context so the link can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: concat(authLink, httpLink),  // Chain the auth link and the http link together
   cache: new InMemoryCache(),
 });
 
